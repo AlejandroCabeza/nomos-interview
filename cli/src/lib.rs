f8ea9b659805de94367b01b6327ff2b82845f5e6
Álex Cabeza Romero <alex93cabeza@gmail.com>
2024-09-30 19:43:24 +0200

    Finish migrate structure to libs (remove CliMessage).

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,7 +1,5 @@
 mod errors;
-pub mod messages;
 pub mod services;
 mod utils;
 
 pub use crate::services::Cli;
-pub use crate::messages::CliMessage;
--- cli/src/messages.rs
+++
@@ -1,9 +0,0 @@
-use overwatch_rs::services::relay::RelayMessage;
-use questions_repository::questions::Question;
-
-#[derive(Debug)]
-pub enum CliMessage {
-    New(Question)
-}
-
-impl RelayMessage for CliMessage {}
